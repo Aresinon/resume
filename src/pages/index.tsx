06089e0c31de4d62e6a6103071874ad17cb081d0
Aresinon <barey13irina@gmail.com>
2023-08-01 14:17:48 +0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { mainData } from '@/src/mainData'
+import Head from 'next/head'
+import { Address } from '../components/Address/Address'
+import { Header } from '../components/Header/Header'
+import { Main } from '../components/Main/Main'
+import { MyLink } from '../components/UI/MyLink/MyLink'
+import { ThemeSwitcher } from '../components/UI/ThemeSwitcher/ThemeSwitcher'
+
+export default function Home() {
+
+   return (
+      <>
+         <Head>
+            <title>Resume | Serafim Barey</title>
+            <meta name='description' content='This is resume of Serafim Barey' />
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <link rel='icon' href='./favicon.svg' />
+         </Head>
+         <Header>
+            <ThemeSwitcher />
+         </Header>
+         <Main mainData={mainData} />
+         <Address>
+            <MyLink href='https://t.me/mifaress' color='beta'>Telegram</MyLink>
+            <MyLink href='mailto:9803211@gmail.com' color='beta'>Email</MyLink>
+         </Address>
+      </>
+   )
+}
